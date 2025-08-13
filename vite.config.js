3a4e779af26552731de11b7643354601fe072353
Salman Sadiq <salmansadiq1015@gmail.com>
2025-08-13 21:28:02 +0500

    dashboard page modules added & menues showing based on modules

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: path.resolve(__dirname, "postcss.config.js"),
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          html2canvas: ["html2canvas"],
-          jsqr: ["jsqr"],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 2000,
+    postcss: "./postcss.config.js",
   },
 });
