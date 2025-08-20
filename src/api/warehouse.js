bb9c6c56697d6e568bc93eb596d93bde971c1559
Asad <asad@retrovgmaes@gmail.com>
2025-08-20 18:18:46 +0500

    manual order

--- src/api/warehouse.js
+++ src/api/warehouse.js
@@ -218,9 +218,4 @@ export async function moveInventoryItem(inventoryId, movedLocationId) {
 }
 
 
-// POST /api/v1/manualOrder/create
-// Body: { customerId, orderNumber, orderDate, orderStatus, orderTotal, orderItems }
-export async function createManualOrder(orderData) {
-  const { data } = await apiClient.post("/api/v1/manualOrder/create", orderData);
-  return data;
-}
+
