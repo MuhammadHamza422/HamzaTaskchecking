597fd2d119a99beb80707bedeed4c7878ec17de4
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-30 23:34:22 +0500

    Latest updates

--- src/pages/inventory/inventory-display.jsx
+++ src/pages/inventory/inventory-display.jsx
@@ -891,7 +891,7 @@ export default function InventoryDisplay({
                           {r?.productData?.pro_title}
                         </td>
                         <td className="px-4 py-3 text-xs whitespace-nowrap font-mono border-b">
-                          {r?.productData.sku || "N/A"}
+                          {r?.productData?.sku || "N/A"}
                         </td>
 
                         <td className="px-4 py-3 text-sm text-gray-500 border-b">
@@ -1084,7 +1084,7 @@ export default function InventoryDisplay({
                             {r?.productData?.pro_title}
                           </div>
                           <div className="mt-1 text-xs text-gray-500 font-mono">
-                            {r?.productData.sku || "N/A"}
+                            {r?.productData?.sku || "N/A"}
                           </div>
                         </div>
                         <div className="text-xs text-gray-600">
@@ -1463,16 +1463,16 @@ export default function InventoryDisplay({
                                 setForm((prev) => ({
                                   ...prev,
                                   productId: p._id,
-                                  productSearch: p.pro_title || p.sku,
+                                  productSearch: p.pro_title || p?.sku,
                                   showProductDropdown: false,
                                   selectedProduct: p, // Store the selected product
                                 }));
                               }}
                               className="px-3 py-2 hover:bg-gray-100 cursor-pointer text-sm border-b last:border-b-0"
                             >
-                              <div className="font-medium">{p.pro_title}</div>
+                              <div className="font-medium">{p?.pro_title}</div>
                               <div className="text-xs text-gray-500">
-                                {p.sku}
+                                {p?.sku}
                               </div>
                             </div>
                           ))
